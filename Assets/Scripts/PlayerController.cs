bfb68903199fb1f9dbba6b0fed0892f99c11c6b0
Neil <neil@premiergsolutions.com>
2021-07-09 21:44:16 +0200

    Added Enemy
    Also added jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -18,16 +16,24 @@ public class PlayerController : MonoBehaviour
     float forwardSpeed = 10;
 
     [SerializeField]
+    float jumpStrength = 10;
+
+    [SerializeField]
     bool logEvents = false;
 
     InputAction move;
     InputAction look;
     InputAction fire;
+    InputAction jump;
 
-    
+    Rigidbody rb;
+
     public bool rotatingClockwise;
     public bool rotatingAntiClockwise;
     public bool movingForwards;
+    public bool firing;
+    public bool jumping;
+
 
 
     // Start is called before the first frame update
@@ -45,6 +51,10 @@ public class PlayerController : MonoBehaviour
         fire = inputActionsMap.FindAction("Fire");
         fire.performed += OnFireEvent;
 
+        jump = inputActionsMap.FindAction("Jump");
+        jump.performed += OnJumpEvent;
+
+        rb = player.GetComponent<Rigidbody>();
     }
 
     private void OnMoveEvent(InputAction.CallbackContext obj)
@@ -99,9 +109,26 @@ public class PlayerController : MonoBehaviour
     {
         var value = obj.ReadValue<float>();
 
+        if (value > 0)
+        {
+            firing = true;
+        }
+
         if (logEvents)
         {
             Debug.Log($"OnFireEvent: Fire is {value}");
+        }
+    }
+
+    private void OnJumpEvent(InputAction.CallbackContext obj)
+    {
+        var value = obj.ReadValue<float>();
+
+        jumping = value > 0;
+
+        if (logEvents)
+        {
+            Debug.Log($"OnJumpEvent: Fire is {value}");
         }
     }
 
@@ -116,17 +143,24 @@ public class PlayerController : MonoBehaviour
         var rotation = new Vector3();
         var translation = new Vector3();
 
+        if (jumping)
+        {
+            rb.AddForce(new Vector3(0, jumpStrength, 0));
+            jumping = false;
+        }
+
+
         if (rotatingClockwise)
         {
             rotation.y += rotationSpeed;
         }
-        
-        if ( rotatingAntiClockwise)
+
+        if (rotatingAntiClockwise)
         {
             rotation.y -= rotationSpeed;
 
         }
-            
+
         player.transform.Rotate(rotation);
 
         if (movingForwards)
